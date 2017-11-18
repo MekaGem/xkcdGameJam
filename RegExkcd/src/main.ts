8b503dbff0427b5947c572649bc2ec8273690e4c
akashin <akashin@google.com>
2017-11-18 12:11:12 +0000

    Add GameState.

--- RegExkcd/src/main.ts
+++ RegExkcd/src/main.ts
@@ -8,9 +8,15 @@ class Card {
     dna_text: createjs.Text;
     container: createjs.Container;
 
+    // Unique card index.
+    id: number;
+
+    static card_count = 0; 
+
     constructor(attack: string, dna: string) {
         this.attack = attack;
         this.dna = dna;
+        this.id = ++Card.card_count;
 
         this.container = new createjs.Container();
 
@@ -50,12 +56,29 @@ class Hand {
     }
 };
 
+class InPlay {
+    cards: Array<Card>;
+    container: createjs.Container;
+
+    constructor(cards: Array<Card>) {
+        this.cards = cards;
+        this.container = new createjs.Container();
+
+        for (let i = 0; i < this.cards.length; ++i) {
+            if (i > 0) {
+                this.cards[i].container.x = this.container.getBounds().width + 15;
+            }
+            this.container.addChild(this.cards[i].container);
+        }
+    }
+};
+
 type PlayedCards = Array<Card>;
 
 const WORDS = ["foo", "bar", "baz", "qux"];
 
-function generate_hand(card_count: number): Hand {
-    let hand = new Array<Card>(card_count);
+function generate_cards(card_count: number): Array<Card> {
+    let cards = new Array<Card>(card_count);
     for (let i = 0; i < card_count; ++i) {
         let attack = WORDS[randomInt(0, WORDS.length - 1)];
         let dna = ""
@@ -63,25 +86,64 @@ function generate_hand(card_count: number): Hand {
         for (let j = 0; j < dna_parts; ++j) {
             dna += WORDS[randomInt(0, WORDS.length - 1)];
         }
-        hand[i] = new Card(attack, dna);
+        cards[i] = new Card(attack, dna);
     }
-    return new Hand(hand);
+    return cards;
 }
+
+const PLAYER_COUNT = 2;
+const FIRST_PLAYER = 0;
+const SECOND_PLAYER = 1;
+
+class GameState {
+    current_player: number;
+    cards_inplay: Array<InPlay>;
+    cards_inhand: Array<Hand>;
+
+    // Container holding cards in hands.
+    hand_containers: Array<createjs.Container>;
+
+    // Container showing cards in play.
+    battlefield_container: createjs.Container;
+
+    constructor(game_field: createjs.Container) {
+        this.current_player = FIRST_PLAYER;
+
+        this.cards_inplay = new Array<InPlay>(PLAYER_COUNT);
+        this.cards_inplay[FIRST_PLAYER] = new Hand(generate_cards(5));
+        this.cards_inplay[SECOND_PLAYER] = new Hand(generate_cards(5));
+
+        this.battlefield_container = new createjs.Container();
+        for (let i = 0; i < PLAYER_COUNT; ++i) {
+            let cards = this.cards_inplay[i].cards;
+            let container = this.cards_inplay[i].container;
+            for (let j = 0; j < cards.length; ++j) {
+                cards[j].container.on("click", (event) => {
+                    this.select_card(i, cards[j].id);
+                });
+            }
+            if (i > 0) {
+                container.y += this.battlefield_container.getBounds().height + 50;
+            }
+            this.battlefield_container.addChild(container);
+        }
+        game_field.addChild(this.battlefield_container);
+    }
+
+
+    select_card(player: number, card_id: number): void {
+        if (player == this.current_player) {
+        } else {
+        }
+    }
+};
 
 function main() {
     let stage = new createjs.Stage('RegExkcdStage');
     stage.mouseEnabled = true;
 
     let game_field = new createjs.Container();
-
-    let first_hand = generate_hand(5);
-    let second_hand = generate_hand(5);
-
-    game_field.addChild(first_hand.container);
-    second_hand.container.y += first_hand.container.getBounds().height + 50;
-    game_field.addChild(second_hand.container);
-
-    // grid_container.cache(0, 0, 640, 480);
+    let game = new GameState(game_field);
     stage.addChild(game_field);
     stage.update();
 
