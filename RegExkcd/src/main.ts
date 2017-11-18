0c68c8067315ac9c885ee6a7548a59c79ba40598
akashin <akashin@google.com>
2017-11-18 21:32:51 +0000

    Move clone_object to utils.

--- RegExkcd/src/main.ts
+++ RegExkcd/src/main.ts
@@ -1,6 +1,6 @@
 import { Card, CardState, generate_cards } from "card";
 import { PlayerState, generate_players } from "player";
-import { randomInt } from "utils";
+import { randomInt, clone_object } from "utils";
 import { TiledLayout, LayoutDirection } from "layout";
 
 class Hand {
@@ -36,10 +36,6 @@ class InPlay {
 const PLAYER_COUNT = 2;
 const FIRST_PLAYER = 0;
 const SECOND_PLAYER = 1;
-
-function clone_object(object) {
-    return JSON.parse(JSON.stringify(object));
-}
 
 class GameState {
     current_player: number;
